81535c9f8b16b12bdeb4eebf1e66227903ee75a9
Lucas Lima <limalucas454@gmail.com>
2021-04-05 18:57:07 +0000

    Adding Champion component

---
+++ draftools/src/components/Champion.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Champion = () => {
+    return (
+        {/* Start: Champion */}
+<div><button className="btn add-champion text-white" type="button">+</button><img className="champion" src="https://i.pinimg.com/originals/bf/ca/5d/bfca5df62dc3ade0416849a26b333f5b.png" />
+  <h1 className="text-center text-white">Top</h1>
+</div>{/* End: Champion */}
+    )
+}
+
+export default Champion
