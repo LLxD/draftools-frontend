30e919101a479897e506af698c4a152d29525fc8
Lucas Lima <limalucas454@gmail.com>
2021-04-05 19:03:46 +0000

    Corrections to champion

--- draftools/src/App.js
+++ draftools/src/App.js
@@ -1,10 +1,16 @@
 import Navbar from './components/Navbar'
+import Champion from './components/Champion'
 
 
 function App() {
   return (
     <div>
         <Navbar/>
+        <Champion role="Top" />
+        <Champion role="Jg" />
+        <Champion role="Mid" />
+        <Champion role="Adc" />
+        <Champion role="Supp" />
     </div>
   );
 }
--- draftools/src/components/Champion.js
+++ draftools/src/components/Champion.js
@@ -1,11 +1,16 @@
 import React from 'react'
 
-const Champion = () => {
+const Champion = (props) => {
     return (
-        {/* Start: Champion */}
-<div><button className="btn add-champion text-white" type="button">+</button><img className="champion" src="https://i.pinimg.com/originals/bf/ca/5d/bfca5df62dc3ade0416849a26b333f5b.png" />
-  <h1 className="text-center text-white">Top</h1>
-</div>{/* End: Champion */}
+<>
+{/* Start: Champion */}
+<div>
+    <button className="btn add-champion text-white" type="button">+</button>
+    <img className="champion" src="https://i.pinimg.com/originals/bf/ca/5d/bfca5df62dc3ade0416849a26b333f5b.png" />
+    <h1 className="text-center text-white">{props.role}</h1>
+</div>
+{/* End: Champion */}
+</>
     )
 }
 
