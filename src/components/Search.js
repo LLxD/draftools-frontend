f295bb690ba296c78b4ecd1c5f3b2dc728274b08
Lucas Lima <limalucas454@gmail.com>
2021-04-16 22:38:31 +0000

    adding initial search structure

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,15 +1,20 @@
 import React from 'react'
 
-const Search = () => {
+function Search(searchString, setSearchString) {
+
+    function handleChange(event) {
+        setSearchString(event.target.value)
+    }
+
     return (
         <div className="row">
             <div className="col">
                 <h2>Pesquisa de campeões</h2>
             </div>
             <div className="col d-flex justify-content-end align-items-center">
-                <input className="form-control mr-sm-2 search" type="search" placeholder="Digite aqui um campeão" aria-label="Search" /><a href="/"><i className="fa fa-search search-icon text-white ml-2" style={{ fontSize: 22 }} /></a>
+                <input value={searchString} onChange={() => handleChange} className="form-control mr-sm-2 search" type="search" placeholder="Digite aqui um campeão" aria-label="Search" />
             </div>
-        </div>
+        </div >
     )
 }
 
