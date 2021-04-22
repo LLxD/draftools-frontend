cc918e957b4b40a7a5ef85d40cf708b1889ec778
Israel Nunes <israel.nunes74@gmail.com>
2021-04-22 11:25:38 -0300

    champions.json adicionado

---
+++ champions/main.js
@@ -0,0 +1,18 @@
+const { request, response } = require('express');
+const express = require('express')
+const app = express();
+
+const { Kayn, REGIONS } = require('kayn')
+
+const kayn = Kayn('RGAPI-4c985925-373c-456b-9e80-c6d71c812723')({
+    region: REGIONS.BRAZIL,
+    apiURLPrefix: 'https://br1.api.riotgames.com/',
+    locale: 'pt_BR',
+})
+
+app.get('/', (req, res) => {
+    kayn.DDragon.Champion.list().then((result) => res.send(result) );
+});
+
+app.listen(5550);
+
