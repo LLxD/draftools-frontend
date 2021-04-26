9ae89c547bdf60708a35945e5326be83f39e1abc
Lucas Lima <limalucas454@gmail.com>
2021-04-26 13:24:16 +0000

    Adding alert component functionality

--- frontend/src/components/Alert.js
+++ frontend/src/components/Alert.js
@@ -1,9 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
+import ChampionIcon from './ChampionIcon';
 
-function Message({ content, type }) {
+function Message({ champion_suggestion, type, addChampion }) {
+
+    const [champion, setChampion] = useState(champion_suggestion[0]);
+
+    const refresh = () => {
+        let item = champion_suggestion[Math.floor(Math.random() * champion_suggestion.length)];
+        setChampion(item);
+    };
+
     return (
-        <div className={`alert alert-${type}`} role="alert">
-            {content}
+        <div className={`mt-5 alert alert-${type}`} role="alert">
+            <h4 className="text-dark">Sugestões de Campeões:</h4>
+            <div className="container">
+                <div className="d-flex justify-content-center row border border-white rounded pb-4">
+                    <ChampionIcon addChampion={addChampion} key={champion.key} id={champion.key} nome={champion.name} tags={""} img={champion.square_image} />
+                </div>
+                <button className="btn btn-block" onClick={() => refresh()}><i class="fa fa-refresh"></i></button>
+            </div>
         </div>
     );
 }
